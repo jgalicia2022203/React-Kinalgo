3cc9a195d7399c80a156db01fa312a1eab24bec0
apalma2022125 <apalma-2022125@kinal.edu.gt>
2024-05-18 04:57:15 -0600

    Se finalizo la estructura del footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,24 +6,27 @@ import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-s
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
+      <div className="footer-top">
+        <h1>If you have a hotel that you want to sell, we sell it for you.</h1>
+        <button className="start-button">Start here...</button>
+      </div>
+      <div className="footer-line"></div>
+      <div className="footer-bottom">
         <div className="footer-section">
-          <img src="../../public/logo.png" alt="Logo" className="footer-logo" />
-          <p className="footer-title">KINALGO</p>
-          <p className="footer-subtitle">YOUR INVESTMENT</p>
-          <p className="footer-details">
-            Tel: + (504) 2276-0010<br />
-            Mov: + (504) 2276-0010<br />
-            Email: infozolutto@zolutto.com
-          </p>
+          <img src="../../public/logo.png" alt="Kinalgo Logo" className="footer-logo" />
+          <h3>KINALGO</h3>
+          <h4>YOUR INVESTMENT</h4>
+          <p>Tel: + (504) 2276-0010</p>
+          <p>Mov: + (504) 2276-0010</p>
+          <p>Email: infozolutto@zolutto.com</p>
           <div className="footer-icons">
-            <FontAwesomeIcon icon={faFacebookF} className="icon" />
-            <FontAwesomeIcon icon={faInstagram} className="icon" />
-            <FontAwesomeIcon icon={faWhatsapp} className="icon" />
+            <FontAwesomeIcon icon={faFacebookF} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faWhatsapp} />
           </div>
         </div>
         <div className="footer-section">
-          <h4>MENU</h4>
+          <h3>MENU</h3>
           <ul>
             <li>Home</li>
             <li>Hotel for Sales</li>
@@ -32,14 +35,14 @@ const Footer = () => {
           </ul>
         </div>
         <div className="footer-section">
-          <h4>OUR PARTNERS</h4>
+          <h3>OUR PARTNERS</h3>
           <ul>
             <li>Hilton</li>
             <li>Marriott</li>
           </ul>
         </div>
-        <div className="footer-section specialized-team">
-          <h4>We have a specialized team of professionals...</h4>
+        <div className="footer-section footer-specialized">
+          <h3>We have a specialized team of professionals...</h3>
           <p>
             Some of us with over 30 years in the hotel industry and others in the real estate, combined makes this a winning team ready to help you. We offer professional advice, maintaining discretion...
           </p>
