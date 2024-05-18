69d4b5bd0aa9d7d891b4ac7beaeb5c9608b71b43
apalma2022125 <apalma-2022125@kinal.edu.gt>
2024-05-17 22:29:21 -0600

    Se crea la base del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-export default function App() {
-  return;
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        {/* Otras rutas pueden ser añadidas aquí */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import HeroSection from '../components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
