840f868fabc4718ddc72bd0852feb0df520a1b90
apalma2022125 <apalma-2022125@kinal.edu.gt>
2024-05-18 03:29:33 -0600

    Se comenzo y se termino el componente de Best Hotels

---
+++ src/components/heroSection1/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './heroSection1.css'; // Importar el archivo CSS desde la nueva ubicación
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section" style={{ backgroundImage: "url('../../public/fondoHero1.jpg')" }}>
+      <div className="overlay"></div>
+      <div className="content">
+        <h1 className="hero-title">IT'S TIME TO ADVENTURES AND EXPERIENCES</h1>
+        <div className="search-container">
+          <div className="input-group">
+            <label>LOCALITATION</label>
+            <input type="text" placeholder="12 STREET LAS VEGAS" className="input"/>
+          </div>
+          <div className="input-group">
+            <label>CHECK IN</label>
+            <input type="date" className="input" value="2024-11-29"/>
+          </div>
+          <div className="input-group">
+            <label>CHECK OUT</label>
+            <input type="date" className="input" value="2024-12-29"/>
+          </div>
+          <button className="search-button">
+            SEARCH <i className="fas fa-search" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
